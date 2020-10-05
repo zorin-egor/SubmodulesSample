04b71160e2764f054f5a13ee511e86f755c46676
zorin-egor <zorin.egor.apps@gmail.com>
2020-10-06 01:53:15 +0300

    Fix code

--- app/src/main/java/com/sample/submodules/MainActivity.kt
+++ app/src/main/java/com/sample/submodules/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sample.submodules
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sample.libchat.ui.views.chat.ChatView
+import com.sample.library.ImDn
 
 class MainActivity : AppCompatActivity() {
+
+    // Sample instance of chat library
+    private lateinit var chatInstance: ChatView
+
+    // Sample instance of image download library
+    private lateinit var imageDownload: ImDn
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
